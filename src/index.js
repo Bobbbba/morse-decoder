dc234d1a12423a9f21e7acb0ee10f9287872e72f
Bobbbba <zwlad@tut.by>
2025-09-05 13:39:36 +0300

    50 passing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,23 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
+module.exports = function decode(expr) {
+  const result = [];
+  for (let i = 0; i < expr.length; i += 10) {
+    result.push(expr.slice(i, i + 10));
+  }
+
+  const result2 = result.map((el) => {
+    return el.replace(/^0+/, '');
+  });
+
+  const result3 = result2.map((el) => (el === '**********' ? el === ' ' : el));
+  const result4 = result3.join().replace(/11/gi, '-');
+  const result5 = result4.replace(/10/gi, '.');
+  const result6 = result5.replace(/,/g, ' ');
+  return result6
+    .split(/ | /)
+    .map((code) => MORSE_TABLE[code] || ' ')
+    .join('');
 };
